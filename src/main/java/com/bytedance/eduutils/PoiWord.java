7b5b6d4194cd4885fec782976581ecd85b315a42
sx89 <sunxu0809@qq.com>
2020-04-13 22:33:25 +0800

    word,excel读写框架

---
+++ src/main/java/com/bytedance/eduutils/PoiWord.java
@@ -0,0 +1,53 @@
+package com.bytedance.eduutils;
+
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+import org.apache.poi.POIXMLDocument;
+import org.apache.poi.POIXMLTextExtractor;
+import org.apache.poi.hwpf.extractor.WordExtractor;
+import org.apache.poi.openxml4j.opc.OPCPackage;
+import org.apache.poi.xwpf.extractor.XWPFWordExtractor;
+
+public class PoiWord {
+    /**
+     * 读取word文件内容
+     *
+     * @param path
+     * @return buffer
+     */
+
+    public String readWord(String path) {
+        String buffer = "";
+        try {
+            if (path.endsWith(".doc")) {
+                InputStream is = new FileInputStream(new File(path));
+                WordExtractor ex = new WordExtractor(is);
+                buffer = ex.getText();
+                ex.close();
+            } else if (path.endsWith("docx")) {
+                OPCPackage opcPackage = POIXMLDocument.openPackage(path);
+                POIXMLTextExtractor extractor = new XWPFWordExtractor(opcPackage);
+                buffer = extractor.getText();
+                extractor.close();
+            } else {
+                System.out.println("此文件不是word文件！");
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return buffer;
+    }
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        PoiWord tp = new PoiWord();
+        String content = tp.readWord("D:\\test01.doc");
+        System.out.println("content====" + content);
+    }
+
+}
---
+++ src/test/java/com/bytedance/eduutils/EduUtilsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bytedance.eduutils;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduUtilsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
